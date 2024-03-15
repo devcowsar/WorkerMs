de2235b6897d4ffd5f310716fb0cbfded807a24f
devcowsar <kawsarcse836@gmail.com>
2024-03-15 10:39:24 +0600

    fix: dashboard_category

--- workerms/src/Components/Category.jsx
+++ workerms/src/Components/Category.jsx
@@ -1,8 +1,17 @@
+import { Box,Grid, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Category = () => {
   return (
-    <div>Category</div>
+    <Grid  container>
+      <Grid item xs={12}>
+         <Box>
+            <Typography variant='h3'>Category List</Typography>
+            <Link ></Link>
+         </Box>
+      </Grid>
+    </Grid>
   )
 }
 
--- workerms/src/Components/Dashboard.jsx
+++ workerms/src/Components/Dashboard.jsx
@@ -1,34 +1,46 @@
 import React from 'react'
-import { Box, Grid, Link, Paper, Stack, Typography } from '@mui/material'
+import { Box, Grid, Paper, Stack, Typography } from '@mui/material'
 import SpeedIcon from '@mui/icons-material/Speed'
 import PeopleIcon from '@mui/icons-material/People';
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import PersonIcon from '@mui/icons-material/Person';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
 
   return (
     <Grid container>
-      <Grid item xs={12} md={3}
+      <Grid item xs={12} md={3.2}
       >
         <Paper square elevation={1} sx={{ backgroundColor: "slategrey", minHeight: "100vh" }}>
           <Stack direction="column" spacing={4} justifyContent="space-between" alignSelf="strech">
-            <Box pt={5} pl={2} component={Link} href="/dashboard" underline="none"><Typography variant='h6' color="whitesmoke" fontWeight={700}>workers management</Typography></Box>
+            <Link to="/dashboard" style={{textDecoration:"none"}}> 
+            <Box pt={5} pl={1} ><Typography variant='h5' color="whitesmoke" fontWeight={700}>workers management</Typography></Box>
+            </Link>
             <Stack pl={4} direction="column" spacing={2}>
-              <Box display="flex" flexDirection="row" alignItems="center" gap={1.5} component={Link} href="/dashboard" underline="none"><Box><SpeedIcon /></Box> <Typography >Dashboard</Typography></Box>
-              <Box display="flex" flexDirection="row" alignItems="center" gap={1.5} component={Link} href="/worker" underline="none"><Box><PeopleIcon /></Box> <Typography >Manage Employees</Typography></Box>
-              <Box display="flex" flexDirection="row" gap={1.5} alignItems="center" component={Link} href="/category" underline="none"><Box><LibraryBooksIcon /></Box> <Typography >Category</Typography></Box>
-              <Box display="flex" flexDirection="row" alignItems="center" gap={1.5} component={Link} href="/dashboard" underline="none"> <Box><PersonIcon /></Box> <Typography >Profile</Typography></Box>
-              <Box display="flex" flexDirection="row" gap={1.5} alignItems="center" component={Link} href="/dashboard" underline="none"> <Box><PowerSettingsNewIcon /></Box> <Typography >Logout</Typography></Box>
+              <Link to="/dashboard" style={{textDecoration:'none'}}>
+              <Box display="flex" flexDirection="row" alignItems="center" gap={1.5}><SpeedIcon fontSize='medium'/> <Typography variant='h6'>Dashboard</Typography></Box>
+              </Link>
+              <Link to="/dashboard/worker" style={{textDecoration: 'none'}}>
+              <Box display="flex" flexDirection="row" alignItems="center" gap={1.5}><PeopleIcon fontSize='medium'/> <Typography variant='h6'>Manage Employees</Typography></Box>
+              </Link>
+              <Link to= "/dashboard/category" style={{ textDecoration: 'none' }}>
+              <Box display="flex" flexDirection="row" gap={1.5} alignItems="center" ><LibraryBooksIcon fontSize='medium'/> <Typography  variant='h6'>Category</Typography></Box>
+              </Link>
+              <Link to="/dashboard/profile" style={{ textDecoration: 'none' }}>
+              <Box display="flex" flexDirection="row" alignItems="center" gap={1.5} > <PersonIcon fontSize="medium"/> <Typography variant='h6'>Profile</Typography></Box>
+              </Link>
+              <Link to="" style={{ textDecoration: 'none' }}>
+              <Box display="flex" flexDirection="row" gap={1.5} alignItems="center"  > <PowerSettingsNewIcon fontSize="medium" /> <Typography variant='h6'>Logout</Typography></Box>
+              </Link>
 
             </Stack>
           </Stack>
         </Paper>
       </Grid>
 
-      <Grid item xs={12} md={9}>
+      <Grid item xs={12} md={8.8}>
         <Paper square>
           <Stack alignItems="center" justifyContent="center" p={1.5}><Typography variant='h4'>Employee Mangement System</Typography></Stack>
         </Paper>
